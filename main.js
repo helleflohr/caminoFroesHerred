0a02bbb3a24764c96e978a902f35887fe8f8d4c5
Johanne Justesen <johannejpoulsen@gmail.com>
2020-05-22 11:41:15 +0200

    fetch nyt

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ export let map = new L.Map("mapid", {
 });
 // window.createMap = () => mapService.createMap();
 
- fetch("http://dittejohannejustesen.dk/wordpress/wordpress-cfh/wp-json/wp/v2/posts")
+ fetch("http://dittejohannejustesen.dk/wordpress/wordpress-cfh/wp-json/wp/v2/posts?_embed")
 .then(function(response) {
     return response.json();
 })
@@ -36,15 +36,15 @@ function appendPosts(posts) {
     for (let post of posts) {
         console.log(post);
         document.querySelector("#grid-posts").innerHTML += `
-<article class="grid-item">
-    <h3>${post.titel.rendered}</h3>
+    <article class="grid-item">
+    <h3>${post.title.rendered}</h3>
     <h4>${post.acf.kilometer}</h4>
     <h5>${post.acf.start}</h5>
     <h5>${post.acf.slut}</h5>
     <p>${post.acf.rutebeskrivelse}</p>
-    <img>${post.acf.billeder}</img>
+    <img src="${post.acf.billeder.url}">
     <p>${post.acf.hvad_siger_andre}</p>
-</article> `
+    </article> `
     }
         
     };

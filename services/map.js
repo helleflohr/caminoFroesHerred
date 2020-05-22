d7cb43b99dab5493a155ccee7c4d448b5e23f4e2
Maja Jensen <eaamnje@students.eaaa.dk>
2020-05-22 10:36:37 +0200

    gpx virker

--- services/map.js
+++ services/map.js
@@ -4,22 +4,26 @@ class MapService {
     // this.loaderService = loaderService;
 
 
+
   }
 
   fetchGeoJson() {
 
-    fetch('geojson/tracks7.geojson')
+    fetch('geojson/Camino-Frøs-Herred-5.gpx')
       .then(function (response) {
-        return response.json();
+        return response.text();
       })
-      .then((json) => {
-        this.createMap(json);
+      .then((gpxData) => {
+        let gpx = new gpxParser();
+        gpx.parse(gpxData);
+        this.createMap(gpx.tracks[0]);
+        // this.drawTrack(gpx.tracks[0]);
 
       });
   }
 
-  createMap(json) {
-    console.log(json)
+  createMap(track) {
+    // console.log(json)
     let layer = new L.StamenTileLayer("terrain");
     let map = new L.Map("mapid", {
       center: new L.LatLng(55.366750, 9.057975),
@@ -30,8 +34,25 @@ class MapService {
     let marker = L.marker([55.366750, 9.057975]).addTo(map);
     marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
-    L.geoJSON(json.features.geometry.coordinates).addTo(map);
+    // L.geoJSON(json.features.geometry.coordinates).addTo(map);
 
+    let coordinates = track.points.map(p => [p.lat.toFixed(5), p.lon.toFixed(5)]);
+
+    var polyline = L.polyline(coordinates, { weight: 6, color: 'darkred' }).addTo(map);
+
+    // zoom the map to the polyline
+    map.fitBounds(polyline.getBounds());
+
+  }
+
+  drawTrack(track) {
+
+    let coordinates = track.points.map(p => [p.lat.toFixed(5), p.lon.toFixed(5)]);
+
+    var polyline = L.polyline(coordinates, { weight: 6, color: 'darkred' }).addTo(map);
+
+    // zoom the map to the polyline
+    map.fitBounds(polyline.getBounds());
   }
 };
 

4d609587731ae4b0cfdb584c88569775d9f9208a
Johanne Justesen <johannejpoulsen@gmail.com>
2020-05-25 13:03:01 +0200

    Flyttet fetch fra js

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,7 @@
 export default class HomePage {
   constructor() {
     this.template();
+    this.fetchDescription();
   }
 
   template() {
@@ -16,4 +17,46 @@ export default class HomePage {
     `;
   }
 
+fetchDescription () {
+  fetch("http://dittejohannejustesen.dk/wordpress/wordpress-cfh/wp-json/wp/v2/posts?_embed&categories=2")
+    .then( (response) => {
+        return response.json();
+    })
+    .then((json) => {
+        this.appendPosts(json)
+    });
+  }
+
+    appendPosts(posts) {
+    for (let post of posts) {
+        console.log(post);
+        document.querySelector("#grid-posts").innerHTML += `
+    
+        <article class="grid-item">
+
+    <section class="backgroundimg">
+    <div id="text-backgroundimg">
+    <div class="title-distance">
+    <h3>${post.title.rendered}</h3>
+    <h4> (${post.acf.distance} km)</h4>
+    </div>
+    <div class="start-end">
+    <h5>${post.acf.startpoint}</h5> 
+    -
+    <h5>${post.acf.endpoint}</h5>
+    </div>
+    </div>
+    </section>
+
+    <section>
+    <p>${post.content.rendered}</p>
+    <img src="${post.acf.images}">
+    <p>${post.acf.crud}</p>
+    </section>
+
+    </article> `
+    }
+
+};
+  
 }

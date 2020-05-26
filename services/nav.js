5ddc18ce7e68336e79777abd99b23adcc2d48eb0
Maja Jensen <eaamnje@students.eaaa.dk>
2020-05-26 09:18:46 +0200

    scroll

--- services/nav.js
+++ services/nav.js
@@ -7,8 +7,7 @@ class ScrollService {
     goTo(number) {
         console.log(fetchService.descriptions)
         console.log(number)
-        let etape = document.querySelector(`#stage${number}`)
-        etape.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
+
         let line = document.getElementsByClassName(`line${number}`)[0];
         line.style.stroke = 'var(--camino-yellow)'
     }
@@ -20,6 +19,8 @@ class ScrollService {
     }
 
     chosen(element) {
+
+
         console.log(element)
         let line = document.getElementsByClassName(`line${element.value}`)[0];
         line.style.stroke = 'var(--camino-yellow)'
@@ -28,6 +29,7 @@ class ScrollService {
 
         // Dropdown
         let etape = document.querySelector(`#stage${element.value}`)
+        etape.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
         let dropdown = etape.getElementsByClassName('dropdown')[0];
         dropdown.style.display = 'block';
 

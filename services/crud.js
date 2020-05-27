1dda151465bd5347346ef2d61d55e63500d65f96
Fjelding <eaahffj@students.eaaa.dk>
2020-05-27 10:59:49 +0200

    Update crud.js

--- services/crud.js
+++ services/crud.js
@@ -1,35 +1,35 @@
 // import { map } from "./../main.js";
 import HomePage from "../pages/home.js"
 class crudService {
-    constructor() {
-        this._dataRef = _db.collection("posts")
-        this._posts;
-        this.read()
-        this.appendPosts(this._posts);
-    };
+  constructor() {
+    this._dataRef = _db.collection("posts")
+    this._posts;
+    this.read()
+    this.appendPosts(this._posts);
+  };
 
 
-// ========== READ ==========
-// 1: data from firebase
-// watch the database ref for changes
-read() {
- _dataRef.onSnapshot(snapshotData => {
-    this._posts = []; // reset _posts
-    snapshotData.forEach(doc => { // loop through snapshotData - like for of loop
-      let post = doc.data(); // save the data in a variable
-      console.log(post); 
-      post.id = doc.id; // add the id to the data variable
-      this._posts.push(post);  // push the data object to the global array  _posts
+  // ========== READ ==========
+  // 1: data from firebase
+  // watch the database ref for changes
+  read() {
+    _dataRef.onSnapshot(snapshotData => {
+      this._posts = []; // reset _posts
+      snapshotData.forEach(doc => { // loop through snapshotData - like for of loop
+        let post = doc.data(); // save the data in a variable
+        console.log(post);
+        post.id = doc.id; // add the id to the data variable
+        this._posts.push(post); // push the data object to the global array  _posts
+      });
     });
-  });
-}
+  }
 
-// append users to the DOM
-appendPosts(posts) {
+  // append users to the DOM
+  appendPosts(posts) {
     let htmlTemplate = "";
     for (let post of posts) {
-    console.log(post.id);
-    console.log(post.name);
+      console.log(post.id);
+      console.log(post.name);
       htmlTemplate += `
       <article>
         <h2>${post.image}</h2>
@@ -44,5 +44,5 @@ appendPosts(posts) {
 
 }
 
-const crudService = new crudService();
+const crudService = new CrudService();
 export default crudService;

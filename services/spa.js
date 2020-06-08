e96dd57c84d34f49bee30a615c02d8eac0991ea5
Fjelding <eaahffj@students.eaaa.dk>
2020-06-08 11:53:23 +0200

    css opdatering

--- services/spa.js
+++ services/spa.js
@@ -16,6 +16,9 @@ class SpaService {
   init() {
     this.pages = document.querySelectorAll(".page");
     this.navItems = document.querySelectorAll(".tabbar a");
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
     this.pageChange();
   }
 
